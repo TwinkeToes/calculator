203e72544c80a819bf0db234cb1a0d27c162f933
TwinkleToes <115041264+TwinkeToes@users.noreply.github.com>
2024-10-21 12:01:01 -0700

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("Button");
+const input = document.getElementById("input");
+
+button.onclick = function () {
+    const answer = input.value
+    console.log(answer)
+};
